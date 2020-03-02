90187f7aeb043f0220c16796c6b0504ff06fb6d0
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-03-02 23:32:36 +0530

    adding to github pages

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,6 +5,6 @@ export const Card = props => (
 	<div className='card-container'>
 		<img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
 		<h1>{props.monster.name}</h1>
-		<p>(props.monster.email}</p>
+		<p>{props.monster.email}</p>
 	</div>
 );
