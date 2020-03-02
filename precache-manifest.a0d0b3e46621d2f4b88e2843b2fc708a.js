d06f381e4030d479d2bb958d6e3580afd3e909cb
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-03-02 23:28:51 +0530

    Updates

---
+++ precache-manifest.a0d0b3e46621d2f4b88e2843b2fc708a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a85ceb75059dae62dfc22ff0802b140",
+    "url": "/monsters-club/index.html"
+  },
+  {
+    "revision": "ad6896511a83bae70021",
+    "url": "/monsters-club/static/css/main.5078a77e.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-club/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-club/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6896511a83bae70021",
+    "url": "/monsters-club/static/js/main.3935fba8.chunk.js"
+  },
+  {
+    "revision": "18537725fa0e5fcb1413",
+    "url": "/monsters-club/static/js/runtime-main.db9e061f.js"
+  }
+]);
